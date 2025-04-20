e4afe3c32c34c902458be63035898a1c4201c0e6
tinystork <134070759+tinystork@users.noreply.github.com>
2025-04-21 00:20:25 +0200

    first stable version

---
+++ localization/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package localization pour Seestar - fournit les fonctionnalités de localisation
+pour l'interface utilisateur.
+"""
+
+from .localization import Localization
+
+__all__ = ['Localization']
