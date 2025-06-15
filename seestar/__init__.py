25046b17fdfda3d10dc64a41aa7b231fb587a3e7
tinystork <zerealtinystork@gmail.com>
2025-06-15 20:34:01 +0200

    version 3.9.0

--- seestar/__init__.py
+++ seestar/__init__.py
@@ -5,7 +5,7 @@ Seestar est conçu pour aligner et empiler des images astronomiques afin
 d'améliorer le rapport signal-bruit des observations astrophotographiques.
 """
 
-__version__ = "3.3.0"  # all set except standard stacking reprojection
+__version__ = "3.9.0"  # all set  stacking reprojection average included
 __author__ = "Tinystork"
 
 # Core functionalities (unchanged from your original structure)
