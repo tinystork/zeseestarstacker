d8ea1b3805a664455a4e821986880d2c1a359692
tinystork <zerealtinystork@gmail.com>
2025-07-07 14:18:25 +0200

    version 5.6.0

--- seestar/gui/mosaic_gui.py
+++ seestar/gui/mosaic_gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
 # import traceback # Décommentez si besoin pour le debug
 import numpy as np 
 import os   # Pour les chemins de fichiers
